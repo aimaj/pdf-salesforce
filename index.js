d64b6d294f9fab97b9aa4d145b435775426625c2
aimaj <aidan.majewski@gmail.com>
2018-10-29 22:34:08 +1100

    adding updated attempts to use frontdoor jsp

--- index.js
+++ index.js
@@ -1,15 +1,8 @@
-/**const puppeteer = require('puppeteer');
 
-(async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto('https://news.ycombinator.com', {waitUntil: 'networkidle2'});
-  await page.pdf({path: 'hn.pdf', format: 'A4'});
-
-  await browser.close();
-})();
-**/
 //https://developer.salesforce.com/docs/atlas.en-us.platform_events.meta/platform_events/platform_events_subscribe_cometd.htm
+
+const puppeteer = require('puppeteer');
+
 
 // Run the adapter code that implements XMLHttpRequest.
 require('cometd-nodejs-client').adapt();
@@ -47,6 +40,83 @@ request.post('https://login.salesforce.com/services/oauth2/token', {
 //https://developer.salesforce.com/docs/atlas.en-us.chatterapi.meta/chatterapi/connect_requests_file_input.htm
 
 
+
+  // Configure the CometD object.
+  cometd.configure({
+    url: body.instance_url + '/cometd/44.0',
+    appendMessageTypeToURL: false,
+    requestHeaders: {
+      'Authorization' : "Bearer " + body.access_token
+    }
+  });
+
+  // Handshake with the server.
+  cometd.handshake(function(h) {
+    console.log(JSON.stringify(h));
+    if (h.successful) {
+        // Subscribe to receive messages from the server.
+        cometd.subscribe('/event/Test__e', function(m) {
+          console.log('server data' + JSON.stringify(m));
+            //var dataFromServer = m.data;
+            // Use dataFromServer.
+
+            //var pageURL = body.instance_url + '/secur/frontdoor.jsp?sid=' + body.access_token + '&retURL=/apex/testPage';
+            var pageURL = body.instance_url + '/apex/testPage';
+            
+            console.log(pageURL);
+            
+            //https://salesforce.stackexchange.com/questions/44799/how-to-access-servlet-rtaimage-resources-over-the-api
+            //use a cookie to authenticate, not frontdoor jsp
+
+            //http://bobbuzzard.blogspot.com/2017/
+            //could do jwt auth on sfdx,
+            
+            /*
+            var child_process=require('child_process');
+            console.log('Getting org details');
+            var orgDetail=JSON.parse(child_process.execFileSync('sfdx', ['force:org:describe', '--json']));
+            var instance=orgDetail.instanceUrl;
+            var token=orgDetail.accessToken;
+            */
+
+
+            /*
+            or redirects? 
+            https://salesforce.stackexchange.com/questions/44799/how-to-access-servlet-rtaimage-resources-over-the-api
+            */
+
+            (async () => {
+              const browser = await puppeteer.launch();
+              const page = await browser.newPage();
+              await page.setCookie({
+                name: 'sid',
+                value: body.access_token,
+                domain: 'c.ap4.salesforce.com',
+                path: '/',
+                expires: 1540812327,
+                httpOnly: false,
+                secure: true
+              });
+              await page.goto(pageURL, {waitUntil: 'networkidle2'});
+              await page.pdf({path: 'test.pdf', format: 'A4'});
+
+              await browser.close();
+            })();
+
+            //https://instance.salesforce.com/secur/frontdoor.jsp?sid=session_ID&retURL=optional_relative_url_to_open
+
+
+
+
+
+
+        });
+    }
+  });
+
+
+
+  //PDF UPLOAD
   /*
   request({
     uri : endpoint,
@@ -66,28 +136,6 @@ request.post('https://login.salesforce.com/services/oauth2/token', {
     console.log(body);
   });
   */
-
-  // Configure the CometD object.
-  cometd.configure({
-    url: body.instance_url + '/cometd/44.0',
-    appendMessageTypeToURL: false,
-    requestHeaders: {
-      'Authorization' : "Bearer " + body.access_token
-    }
-  });
-
-  // Handshake with the server.
-  cometd.handshake(function(h) {
-    console.log(JSON.stringify(h));
-    if (h.successful) {
-        // Subscribe to receive messages from the server.
-        cometd.subscribe('/event/Test__e', function(m) {
-          console.log('server data' + JSON.stringify(m));
-            var dataFromServer = m.data;
-            // Use dataFromServer.
-        });
-    }
-  });
 
 });
 
